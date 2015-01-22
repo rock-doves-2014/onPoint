6eafef2d4150b7da97c035f2a3a475f2f19334d0
Luke Thomas <lukert33@gmail.com>
2015-01-22 16:50:31 -0500

    Controller; define major SessionController properties.

--- controllers/sessioncontroller.js
+++ controllers/sessioncontroller.js
@@ -3,3 +3,27 @@ function SessionController(pointUser){
   this.user = pointUser;
 }
 
+SessionController.prototype.shortenUrl = function(){
+  if (this.user.alwaysAddUrl){
+    var tabUrl = chrome.tabs.query({
+      active: true, currentWindow: true}, function(tabs){
+      var tab = tabs[0];
+      var url = tab.url;
+    })
+
+    console.log(url + "*****minify me!");
+    return "bitlyurl"; //TK; use above url
+  } else {return ""};
+}
+
+SessionController.prototype.pause = function(){
+  this.enabled = false;
+}
+
+SessionController.prototype.echoListener = function(){
+  //watch the page for highlighting w/ cursor TK
+}
+
+SessionController.prototype.spawnEchoForm = function(){
+  // build the form anchored on the highlighting TK
+}
