dffe40660870515f628f47b752529ebe4aedf280
LukeKedz <lukekedziora@gmail.com>
2015-01-23 15:41:17 -0500

    highlighting text, writing text, and capturing the data in console

--- dummyformreader.js
+++ dummyformreader.js
@@ -1,22 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  function Echo(string, userText){
+    this.quote = string;
+    this.userText = userText || "";
+    // this.sentTo = [];
+    // this.bitly = SessionController.shortenUrl();
+    // this.wasSent = false;
+  }
+
   document.onmouseup = function run() {
 
     if (window.getSelection() != "") {
 
-
       var echoForm = document.getElementById("echo-submit");
       console.log(echoForm);
 
-      selectedString = window.getSelection().toString()
+      var selectedString = window.getSelection().toString();
       console.log(selectedString);
 
-      // chrome.runtime.sendMessage({message: selectedString}, function(response) {
+      var userText = document.getElementById("userText").value;
+      console.log(userText);
 
-      // });
+      var echo = new Echo(selectedString, userText);
 
       echoForm.addEventListener("submit", function(){
         console.log("form submitted");
+        console.log("creating Echo object... ");
+
+
+
+        // chrome.runtime.sendMessage({message: selectedString}, function(response) {
+        // });
+
       });
     }
   };
