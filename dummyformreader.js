f333c4f51f5149b0edda7015cb0d312216c2bea4
LukeKedz <lukekedziora@gmail.com>
2015-01-23 15:26:20 -0500

    initial setup of existing components

--- dummyformreader.js
+++ dummyformreader.js
@@ -1,9 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var echoForm = document.getElementById("echo-submit");
 
-  echoForm.addEventListener("submit", function(){
-      console.log("form submitted");
-    });
+  document.onmouseup = function run() {
+
+    if (window.getSelection() != "") {
+
+
+      var echoForm = document.getElementById("echo-submit");
+      console.log(echoForm);
+
+      selectedString = window.getSelection().toString()
+      console.log(selectedString);
+
+      // chrome.runtime.sendMessage({message: selectedString}, function(response) {
+
+      // });
+
+      echoForm.addEventListener("submit", function(){
+        console.log("form submitted");
+      });
+    }
+  };
+
+
+
+
+
 });
 
 
