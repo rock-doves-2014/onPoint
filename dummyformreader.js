4b68dbeb903224ef7a0c09ba4c136d9760326ced
LukeKedz <lukekedziora@gmail.com>
2015-01-23 16:23:08 -0500

    capturing x and y coordinates on mouseup

--- dummyformreader.js
+++ dummyformreader.js
@@ -2,11 +2,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   var session = new SessionController();
 
-  document.onmouseup = function run() {
+  document.onmouseup = function run(event) {
 
     if (window.getSelection() != "") {
 
-      var element = session.spawnEchoForm();
+      var xCoord = event.pageX;
+      var yCoord = event.pageY;
+
+      var element = session.spawnEchoForm(xCoord, yCoord);
 
       var echoForm = document.getElementById("echo-submit");
       var selectedString = window.getSelection().toString();
