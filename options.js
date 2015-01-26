103d8f796d33a09b5bf1ad3a75e64e8975d73d88
Jorge Rubio <jorgerc85@gmail.com>
2015-01-26 16:48:18 -0500

    Options.js: Rename file to options.js

--- options.js
+++ options.js
@@ -2,3 +2,38 @@ document.addEventListener('DOMContentLoaded', function() {
   restore_options();
   document.getElementById('myonoffswitch').addEventListener('click', save_options);
 });
+
+function save_options() {
+  var twitterSwitch = document.getElementById("twitterOn").checked;
+  var faceBookSwitch = document.getElementById("facebookOn").checked;
+  var fbFloorSwitch = document.getElementById("facebookCharFloor").checked;
+  var alwaysUrlSwitch = document.getElementById("alwaysAddUrl").checked;
+
+  chrome.storage.sync.set({
+    twitterOn: tw,
+    facebookOn: fb,
+    facebookCharFloor: fbFloor,
+    alwaysAddUrl: url
+
+  }, function() {
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved!';
+
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+    //send key value pairs of user's changed options to update. Make async.
+  }, function(items) {
+    //Luke Kedz wrote this callback. It should act like a switch..case for checked
+    //boxes.
+    document.getElementById("twitterOn").checked = items.twitterOn;
+    document.getElementById("facebookOn").checked = items.facebookOn;
+    document.getElementById("facebookCharFloor").checked = items.facebookCharFloor;
+    document.getElementById("alwaysAddUrl").checked = items.alwaysAddUrl;
+  });
+};
