c4c785321035bc0892185b2b837a299214ae39bb
Luke Kedziora <lukekedziora@gmail.com>
2015-01-25 23:34:44 -0500

    updating event page.js

--- eventpage.js
+++ eventpage.js
@@ -1,3 +1,13 @@
-function createEcho(selectedString, userText, url){
-  var echo = new Echo(selectedString, userText, url);
-}
+chrome.runtime.onMessage.addListener(
+  function(echo, sender, sendResponse) {
+
+    var message = JSON.parse(echo);
+
+    var xml = new XMLHttpRequest();
+    xml.open("POST", "http://localhost:3000/json", true);
+    xml.send(message);
+
+    sendResponse({
+      message: "Message: " + echo.message
+    });
+});
