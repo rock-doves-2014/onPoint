0999423e20ddcee2c6d04893d137ccfa35efd230
Luke Kedziora <lukekedziora@gmail.com>
2015-01-25 23:56:33 -0500

    editing to stringify, not parse

--- eventpage.js
+++ eventpage.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
   function(echo, sender, sendResponse) {
 
-    // var message = JSON.parser(echo);
+    var message = JSON.stringify(echo);
 
     var xml = new XMLHttpRequest();
     xml.open("POST", "http://localhost:3000/json", true);
