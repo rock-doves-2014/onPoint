d46ef99d4db7113abfc27345b0b2224917230396
Jorge Rubio <jorgerc85@gmail.com>
2015-01-27 14:19:02 -0500

    EventPage.js: Add google_credentials and chrome_token to requests sent to Rails

--- eventpage.js
+++ eventpage.js
@@ -1,19 +1,28 @@
 chrome.runtime.onMessage.addListener(
   function(echo, sender, sendResponse) {
-
-    var message = JSON.stringify(echo);
-
-    var xml = new XMLHttpRequest();
-    xml.open("POST", "http://localhost:3000/api/echos", true);
-    xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xml.send(message);
-
-    sendResponse({
-      message: "Message: " + message
+    var promise = new Promise(function(resolve, reject) {
+      chrome.identity.getProfileUserInfo(function(userInfo) {
+        echo['google_credentials'] = userInfo.id;
+      });
+      chrome.storage.local.get('chrome_token', function(items) {
+        echo['chrome_token'] = items.chrome_token
+      });
+      var timer = setInterval(function() {
+        if (echo['google_credentials'] != null && echo['chrome_token'] != null) {
+          resolve(echo);
+          clearInterval(timer);
+        }
+      }, 100)
+    }).then(function(value) {
+      var message = JSON.stringify(echo);
+      var xml = new XMLHttpRequest();
+      xml.open("POST", "http://localhost:3000/api/echos", true);
+      xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xml.send(message);
+      sendResponse({
+        message: "Message: " + message
+      }, function(reason) {
+      });
     });
-});
-
-chrome.identity.getProfileUserInfo(function(userInfo) {
-
-})
-
+  }
+);
