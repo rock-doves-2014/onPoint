6e3b066af0002acca72843e445c975c17aed1365
Luke Thomas <lukert33@gmail.com>
2015-01-28 17:07:57 -0500

    Add our heroku url throughout.

--- eventpage.js
+++ eventpage.js
@@ -17,7 +17,7 @@ chrome.runtime.onMessage.addListener(
     }).then(function(value) {
       var message = JSON.stringify(echo);
       var xml = new XMLHttpRequest();
-      xml.open("POST", "http://localhost:3000/api/echos", true);
+      xml.open("POST", "https://echoko.herokuapp.com/api/echos", true);
       xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
       xml.send(message);
       sendResponse({
