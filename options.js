81258588725ebbf99b54d0bed6358d50ee9fa2a0
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-28 11:15:23 -0500

    Options.js: Delete unused logic

--- options.js
+++ options.js
@@ -1,46 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  restore_options();
   userRailsOauth();
-  document.getElementById('myonoffswitch').addEventListener('click', function() {
-    save_options();
-  });
   document.getElementById('TwitterOauth').addEventListener('click', function(event) {
     event.preventDefault();
     RailsTwitterOauth();
   });
 });
-
-function save_options() {
-  // var twitterSwitch = document.getElementById("twitterOn").checked;
-  // var faceBookSwitch = document.getElementById("facebookOn").checked;
-  // var fbFloorSwitch = document.getElementById("facebookCharFloor").checked;
-  // var alwaysUrlSwitch = document.getElementById("alwaysAddUrl").checked;
-
-  chrome.storage.sync.set({
-    twitterOn: twitterSwitch,
-    facebookOn: faceBookSwitch,
-    facebookCharFloor: fbFloorSwitch,
-    alwaysAddUrl: alwaysUrlSwitch
-  }, function() {
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved!';
-
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-}
-
-function restore_options() {
-  chrome.storage.sync.get(null, function(items) {
-    //Luke Kedz wrote this callback. It should act like a switch..case for checked
-    //boxes.
-    document.getElementById("twitterOn").checked = items.twitterOn;
-    document.getElementById("facebookOn").checked = items.facebookOn;
-    document.getElementById("facebookCharFloor").checked = items.facebookCharFloor;
-    document.getElementById("alwaysAddUrl").checked = items.alwaysAddUrl;
-  });
-};
 
 function userRailsOauth() {
   chrome.identity.getProfileUserInfo(function(userInfo) {
@@ -55,12 +19,6 @@ function userRailsOauth() {
           var responseString = JSON.parse(xml.response);
           chrome.storage.sync.set({
             'chrome_token': responseString.key
-          }, function() {
-            var status = document.getElementById('status');
-            status.textContent = 'Options saved!';
-            setTimeout(function() {
-              status.textContent = '';
-            }, 750);
           });
         } else {
           reject("Your response was bad.")
