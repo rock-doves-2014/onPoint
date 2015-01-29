ecb7f8b59cd620e6614ef6328932a3f7d1f0bb47
Jorge Rubio C <jorgerc85@gmail.com>
2015-01-28 22:32:45 -0500

    upd form propogation values

--- contentscript.js
+++ contentscript.js
@@ -148,10 +148,11 @@ function spawnEchoForm(x, y, that, selectedString) {
     x = document.body.clientWidth - 400;
     that.echoForm.style.left = x + "px";
   } else {
+    x = x - 25;
     that.echoForm.style.left = x + "px";
   };
 
-  y = y + 15;
+  y = y + 20;
   that.echoForm.style.top = y + "px";
 
   body = document.getElementsByTagName("body")[0];
